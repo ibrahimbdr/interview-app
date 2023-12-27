42c6b2cce4e5d9a41cd0e1dd34b78dd7268abb24
ibrahimbdr <ibrahimbdr@outlook.com>
2023-12-27 21:09:20 +0200

    update

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const mongoose = require("mongoose");
 var jwt = require('jsonwebtoken');
 var uuid4 = require('uuid4');
 var cors = require('cors');
+const {Storage} = require('@google-cloud/storage');
+var fs = require('fs');
+var path = require('path');
 var app = express();
 app.use(express.json());
 
 app.use(cors());
+const storage = new Storage({
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
+  });
 
 const serviceAccount = require('./live-video.json');
 
@@ -29,6 +35,48 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
 
+const questionVideoFileSchema = new mongoose.Schema({
+  name: String,
+  number: Number
+}, { strict: false });
+
+const QuestionVideoFile = mongoose.model("QuestionVideoFile", questionVideoFileSchema);
+
+async function downloadFileFromGoogleCloud(roomId, date, destinationFileName) {
+    const prefix = `interview_app/thirdparty_recording_test/beam/${roomId}/${date}/`;
+  
+    const options = {
+      prefix: prefix,
+    };
+  
+    const [files] = await storage.bucket(process.env.GOOGLE_APPLICATION_BUCKET).getFiles(options);
+  
+    files.forEach(file => {
+      if (file.name.startsWith(prefix + 'Rec')) {
+        const options = {
+          destination: destinationFileName,
+        };
+  
+        file.download(options).then(() => {
+          console.log(`Blob ${file.name} downloaded to ${destinationFileName}.`);
+        });
+      }
+    });
+  }
+
+  async function uploadFileToFirebase(sourceFileName, FirebaseDestinationFileName) {
+    await bucket.upload(sourceFileName, {
+      destination: FirebaseDestinationFileName,
+      gzip: true,
+      metadata: {
+        cacheControl: 'public, max-age=31536000',
+      },
+    });
+  
+    console.log(`${sourceFileName} uploaded to ${destinationFileName}.`);
+  }
+  
+  
 
 app.get('/generateManagementToken', function(req, res) {
     var app_access_key = process.env.APP_ACCESS_KEY;
@@ -64,17 +112,34 @@ const logSchema = new mongoose.Schema({}, { strict: false });
 const Log = mongoose.model("Log", logSchema);
 
 app.post('/generateStreamingLogs', async (req, res) => {
-  const logDocument = new Log(req.body);
-
-  try {
-    await logDocument.save();
-    res.status(200).send('Log saved successfully');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Failed to save log');
-  }
-});
-
+    const logDocument = new Log(req.body);
+    if(req.body.type === 'beam.stopped.success'){
+      roomId = req.body.data.room_id;
+      const questionCount = await QuestionVideoFile.countDocuments({});
+      const nextNumber = questionCount + 1;
+      const fileName = `Q_${nextNumber}`;
+      const questionVideoFileDocument = new QuestionVideoFile({ name: fileName, number: nextNumber });
+      await questionVideoFileDocument.save();
+      const destinationFileName = path.join(__dirname, 'interview_version', 'videos', fileName);
+      const logFileName = path.join(__dirname, 'interview_version', 'logs', `${fileName}.log`);
+      await downloadFileFromGoogleCloud(roomId, date, destinationFileName).catch(console.error);
+      fs.appendFileSync(logFileName, JSON.stringify(req.body) + "\n################################################\n");
+      const firebaseLogFileName = path.join('interview_version', 'logs', `${fileName}.log`);
+      const firebaseVideoFileName = path.join('interview_version', 'videos', fileName);
+      await uploadFileToFirebase(logFileName, firebaseLogFileName).catch(console.error);
+      await uploadFileToFirebase(destinationFileName, firebaseVideoFileName).catch(console.error);
+      fs.unlinkSync(logFileName);
+      fs.unlinkSync(destinationFileName);
+    }
+    try {
+      await logDocument.save();
+      res.status(200).send('Log saved successfully');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Failed to save log');
+    }
+  });
+  
 
 const port = process.env.PORT || 4242;
 app.listen(port, () => {
